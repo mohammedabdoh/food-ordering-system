b5ef025e9dd703f91dc1c31e3605aa2c4b2f4c0c
Mohammed Abdoh <mohammed.abdoh@flixbus.com>
2023-04-08 14:24:14 +0200

    Replace the DomainEvent interface with an abstract class

--- common/common-domain/src/main/java/com/food/ordering/system/domain/event/DomainEvent.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/event/DomainEvent.java
@@ -1,5 +1,21 @@
 package com.food.ordering.system.domain.event;
 
-public interface DomainEvent<T> {
+import java.time.ZonedDateTime;
 
+abstract public class DomainEvent<T> {
+    private final T entity;
+    private final ZonedDateTime createdAt;
+
+    public DomainEvent(T entity, ZonedDateTime createdAt) {
+        this.entity = entity;
+        this.createdAt = createdAt;
+    }
+
+    public T getEntity() {
+        return entity;
+    }
+
+    public ZonedDateTime getCreatedAt() {
+        return createdAt;
+    }
 }
